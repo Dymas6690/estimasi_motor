3543e43101b3c49627e01d1572e437e742c5c4d8
Dymas6690 <dymaspratama9h@gmail.com>
2024-11-06 12:35:32 +0700

    Update estimasi_motor.py

--- estimasi_motor.py
+++ estimasi_motor.py
@@ -1,7 +1,7 @@
 import pickle
 import streamlit as st
 
-model = pickle.load(open('estimasi_motor.sav', 'rb'))
+model = pickle.load(open('models/estimasi_motor.sav', 'rb'))
 
 st.title('Estimasi harga motor bekas')
 
